8951477de4a338ba22014c2080a5d3881fbfa895
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-06 01:29:33 +0530

    js commit 01

---
+++ JavaScript_Test_Solutions/js_test_1.js
@@ -0,0 +1,15 @@
+// ARITHMETIC OPERATIONS
+
+import {arithmeticOperation} from "mathinfinity";
+
+console.log(`Addition: ${arithmeticOperation(10, 20, "+")}`); // Addition: 30
+console.log(`Subtraction: ${arithmeticOperation(50, 20, "-")}`); // Subtraction: 30
+console.log(`Multiplication: ${arithmeticOperation(30, 40, "*")}`); // Multiplication: 1200
+console.log(`Division: ${arithmeticOperation(90, 20, "/")}`); // Division: 4.5
+console.log(`Modulo: ${arithmeticOperation(100, 17, "%")}`); // Modulo: 15
+
+console.log(`Addition: ${arithmeticOperation("10", 20, "+")}`); // Addition: 30
+console.log(`Subtraction: ${arithmeticOperation(50, "20", "-")}`); // Subtraction: 30
+console.log(`Multiplication: ${arithmeticOperation('30', 40, "*")}`); // Multiplication: 1200
+console.log(`Division: ${arithmeticOperation(90, '20', "/")}`); // Division: 4.5
+console.log(`Modulo: ${arithmeticOperation(`100`, 17, "%")}`); // Modulo: 15
