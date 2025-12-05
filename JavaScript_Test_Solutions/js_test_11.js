890d7dcf91d13ca89c2f69c6e29bf756ab4ad9dc
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-06 03:42:30 +0530

    js commit 07

---
+++ JavaScript_Test_Solutions/js_test_11.js
@@ -0,0 +1,9 @@
+// DIVISION OPERATIONS WITH GIVEN INPUTS AND CHOICE
+
+import {divisionOperation} from "mathinfinity";
+
+console.log(`Quotient: ${divisionOperation(14, '8', 3, 'QUOTIENT')}`); // Quotient: 2
+console.log(`Remainder: ${divisionOperation(140, 8, "3", "Remainder")}`); // Remainder: 116
+console.log(`Dividend: ${divisionOperation(`250`, 28, "17", "DiViDeNd")}`); // Dividend: 7017
+console.log(`Divisior: ${divisionOperation(360, 17, "20", "dIvIsOr")}`); // Divisior: 20
+
---
+++ JavaScript_Test_Solutions/js_test_12.js
@@ -0,0 +1,9 @@
+// DIVISION OPERATIONS WITH GIVEN INPUTS AND CHOICE
+
+import {division} from "mathinfinity";
+
+console.log(`Quotient: ${division(117, '13', 'QUOTIENT')}`); // Quotient: 9
+console.log(`Remainder: ${division(188, "23", "Remainder")}`); // Remainder: 4
+console.log(`Dividend: ${division(`111`, `24`, "DiViDeNd")}`); // Dividend: 111
+console.log(`Divisior: ${division("150", '7', "dIvIsOr")}`); // Divisior: 7
+
