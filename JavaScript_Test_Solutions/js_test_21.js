80403967a35741de889947e5cd19604a11bb202a
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-07 06:25:07 +0530

    js commit 11

---
+++ JavaScript_Test_Solutions/js_test_21.js
@@ -0,0 +1,6 @@
+// PRINT THE EVEN OR ODD NUMBERS FROM 0 TO RANGE
+
+import {evenoddZeroRange} from "mathinfinity";
+
+evenoddZeroRange(20, "even"); // 0 2 4 6 8 10 12 14 16 18 20
+evenoddZeroRange("20", "odd"); // 1 3 5 7 9 11 13 15 17 19
---
+++ JavaScript_Test_Solutions/js_test_22.js
@@ -0,0 +1,9 @@
+// PRINT THE EVEN OR ODD NUMBERS FROM 0 TO RANGE
+
+import {evenoddzeroRangeArry} from "mathinfinity";
+
+let res1 = evenoddzeroRangeArry(20, "even");
+console.log(res1); // [ 0,  2,  4,  6,  8, 10, 12, 14, 16, 18, 20 ]
+
+let res2 = evenoddzeroRangeArry(20, "odd");
+console.log(res2); // [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19 ]
