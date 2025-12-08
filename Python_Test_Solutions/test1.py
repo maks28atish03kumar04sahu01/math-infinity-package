276a5059364571d3c76dff76052aac4c1abb8ff5
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-08 05:38:40 +0530

    python commit 01

---
+++ Python_Test_Solutions/test1.py
@@ -0,0 +1,16 @@
+from mathinfinity import *
+
+result1 = arithmeticoperation(10, 20, "+")
+print(f"Addition: {result1}")
+
+result2 = arithmeticoperation(20, '10', "-")
+print(f"Subtraction: {result2}")
+
+result3 = arithmeticoperation("40", 30, "*")
+print(f"Multiplication: {result3}")
+
+result4 = arithmeticoperation('123', "17", "/")
+print(f"Division: {result4}")
+
+result5 = arithmeticoperation("289", '26', "%")
+print(f"Modulo: {result5}")
