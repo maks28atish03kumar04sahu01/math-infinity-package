662e0fad46fa7d654553ba62ed646d9e2f9a43c0
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-08 06:30:08 +0530

    python commit 04

---
+++ Python_Test_Solutions/test4.py
@@ -0,0 +1,9 @@
+from mathinfinity import *
+
+printzerorange(10)
+printzerorange("20")
+printzerorange('30')
+
+print(printzerorangearray(10))
+print(printzerorangearray("20"))
+print(printzerorangearray('30'))
