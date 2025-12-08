5a5350e514a99048a759f524a380189188299556
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-08 06:57:27 +0530

    python commit 05

---
+++ Python_Test_Solutions/test5.py
@@ -0,0 +1,11 @@
+from mathinfinity import *
+
+printrange(10, 20)
+printrange(30, '40')
+printrange("50", 60)
+printrange('70', "80")
+
+print(printrangearray(10, 20))
+print(printrangearray(30, "40"))
+print(printrangearray('50', 60))
+print(printrangearray('60', "70"))
