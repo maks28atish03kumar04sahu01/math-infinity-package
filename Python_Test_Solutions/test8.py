5cf4a9384008772985a898752607f3e6036df93c
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-27 15:38:12 +0530

    python commit 08

---
+++ Python_Test_Solutions/test8.py
@@ -0,0 +1,6 @@
+from mathinfinity import *
+
+print(division(117, '13', 'QUOTIENT'))
+print(division(188, "23", "Remainder"))
+print(division(111, '24', "DiViDeNd"))
+print(division("150", '7', "dIvIsOr"))
