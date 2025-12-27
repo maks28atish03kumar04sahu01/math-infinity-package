ade6ef5f780c442ab8120eb8768ee86e951e9f80
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-27 15:37:51 +0530

    python commit 07

---
+++ Python_Test_Solutions/test7.py
@@ -0,0 +1,6 @@
+from mathinfinity import *
+
+print(divisionoperation(14, '8', 3, "Quotient"))
+print(divisionoperation(140, 8, "3", "Remainder"))
+print(divisionoperation('250', 28, "17", "DiViDeNd"))
+print(divisionoperation(360, 17, "20", "dIvIsOr"))
