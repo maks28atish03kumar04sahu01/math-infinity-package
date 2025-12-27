70c3e6e04a184c46d9b2bb6a77743bc10607f354
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-27 15:41:04 +0530

    python commit 10

---
+++ Python_Test_Solutions/test10.py
@@ -0,0 +1,7 @@
+from mathinfinity import *
+
+print(checkDivisibilityArray(6, [10, 20, 30,]))
+print(checkDivisibilityArray(5, 20, 25, 30, 37))
+
+print(checkNonDivisibilityArray(6, [10, 20, 30,]))
+print(checkNonDivisibilityArray(5, 20, 25, 30, 37))
