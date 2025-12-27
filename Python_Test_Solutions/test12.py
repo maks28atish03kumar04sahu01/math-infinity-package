9571a560d387e45d1cfdafc8e8c1f8104fafcfe6
Atish Kumar Sahu <160933920+maks28atish03kumar04sahu01@users.noreply.github.com>
2025-12-27 15:42:01 +0530

    python commit 12

---
+++ Python_Test_Solutions/test12.py
@@ -0,0 +1,6 @@
+from mathinfinity import *
+
+print(checkDivisibilitySeriesArray(50, 12))
+print(checkNonDivisibilitySeriesArray(50, 15))
+print(checkDivisibilityRangeArray(50, 90, 12))
+print(checkNonDivisibilityRangeArray(50, 70, 15))
